a7958f1d5f4961eea6f8c6b25923db4a23e24a30
Maxime Moreillon <m.moreillon@gmail.com>
2020-10-22 13:29:02 +0000

    initial

---
+++ main.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const httpProxy = require('http-proxy')
+
+dotenv.config()
+
+const PORT = process.env.PORT || 80
+
+const app = express()
+
+const proxy = httpProxy.createProxyServer()
+
+let handle_proxy = (req, res, options) => {
+  proxy.web(req, res, options, (error) => {
+    res.status(500).send(`The proxy failed to retrieve resource at ${process.env.PROXY_ROOT}`)
+    console.log(error)
+  })
+}
+
+app.all('/proxy/:service_name*', (req,res) => {
+
+  const service_name_formatted = req.params.service_name.toUpperCase().replace('-','_')
+  const target_hostname = process.env[`PROXY_${service_name_formatted}`]
+
+  if(!target_hostname) {
+    res.status(404).send(`The Proxy is not configured to handle the service called '${service_name}'`)
+  }
+
+  const original_path = req.originalUrl
+
+  // manage_path
+  let split_string = original_path.split('/')
+  split_string.splice(1,2)
+  const new_path =  split_string.join('/')
+
+  // Assemble the target_url
+  const target_url = `${target_hostname}${new_path}`
+
+  const proxy_options = { target: target_url, ignorePath: true}
+
+  handle_proxy(req, res, proxy_options)
+})
+
+app.get('/*', (req, res) => {
+  // The route used for the front end
+  const proxy_options = { target: process.env.PROXY_ROOT}
+  handle_proxy(req, res, proxy_options)
+})
+
+app.listen(PORT, () => {
+  console.log(`API proxy listening on port ${PORT}`)
+})
