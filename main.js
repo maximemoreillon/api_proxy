9896f7b0430583f072c495bd4d385536d84e1e70
Maxime Moreillon <m.moreillon@gmail.com>
2020-12-02 01:29:25 +0000

    code readability

--- main.js
+++ main.js
@@ -43,8 +43,6 @@ app.all('/proxy/:service_name*', (req,res) => {
   const service_name_formatted = service_name.toUpperCase().replace('-','_')
   const target_hostname = process.env[`PROXY_${service_name_formatted}`]
 
-
-
   if(!target_hostname) {
     return res.status(404).send(`The Proxy is not configured to handle the service called '${service_name}'`)
   }
@@ -75,15 +73,8 @@ app.all('/socket.io', (req, res) => {
 
 app.get('/*', (req, res) => {
   // The route used for the front end
-
-  // TODO: Try to merge with previous
-
   const target = process.env.PROXY_ROOT
-
-  // IgnorePath: false because we use the path as provided
-  const proxy_options = { target, ignorePath: false}
-
-  handle_proxy(req, res, proxy_options)
+  handle_proxy(req, res, { target })
 })
 
 
