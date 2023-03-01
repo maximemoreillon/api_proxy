78b543b49f5ca9b5ce0a02d1b5bb12cc000992b1
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-02 08:43:25 +0900

    fixing tdd

--- test/app.js
+++ test/app.js
@@ -1,56 +1,49 @@
 const request = require("supertest")
 const { expect } = require("chai")
-const  { app } = require("../main.js")
+const { app } = require("../")
 
-describe('/proxy', () => {
+describe("/proxy", () => {
+  before(async () => {
+    //console.log = function () {};
+  })
 
-    before(async () => {
-        //console.log = function () {};
+  describe("GET /proxy", () => {
+    it("Should return app info", async () => {
+      const { status } = await request(app).get("/proxy")
+
+      expect(status).to.equal(200)
     })
+  })
 
-    describe("GET /proxy", () => {
-        it("Should return app info", async () => {
-            const { status } = await request(app).get('/proxy')
+  describe("GET /", () => {
+    it("Should return GUI", async () => {
+      const { status } = await request(app).get("/")
 
-            expect(status).to.equal(200)
-        })
+      expect(status).to.equal(200)
     })
+  })
 
+  describe("GET /proxy/test", () => {
+    it("Should proxy the test service", async () => {
+      const { status } = await request(app).get("/proxy/test-valid")
 
-    describe("GET /", () => {
-        it("Should return GUI", async () => {
-            const { status } = await request(app).get('/')
-
-            expect(status).to.equal(200)
-        })
+      expect(status).to.equal(200)
     })
+  })
 
-    describe("GET /proxy/test", () => {
-        it("Should proxy the test service", async () => {
-            const { status } = await request(app).get('/proxy/test-valid')
+  describe("GET /proxy/unregistered", () => {
+    it("Should not allow proxying an unregistered service", async () => {
+      const { status } = await request(app).get("/proxy/unregistered")
 
-            expect(status).to.equal(200)
-        })
+      expect(status).to.equal(404)
     })
+  })
 
-    describe("GET /proxy/unregistered", () => {
-        it("Should not allow proxying an unregistered service", async () => {
-            const { status } = await request(app).get('/proxy/unregistered')
+  describe("GET /proxy/test", () => {
+    it("Should not proxy a service which does not work", async () => {
+      const { status } = await request(app).get("/proxy/test-invalid")
 
-            expect(status).to.equal(404)
-        })
+      expect(status).to.equal(500)
     })
-
-    describe("GET /proxy/test", () => {
-        it("Should not proxy a service which does not work", async () => {
-            const { status } = await request(app).get('/proxy/test-invalid')
-
-            expect(status).to.equal(500)
-        })
-    })
-
-
-
-
+  })
 })
-
