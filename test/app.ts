af78d181ec615e4de668ab744ad214823dbd549f
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-04 15:23:11 +0900

    fixing tests

--- test/app.ts
+++ test/app.ts
@@ -1,11 +1,11 @@
-import request from "supertest"
-import { expect } from "chai"
-import { app } from ".."
-
-process.env.PATH_PREFIX = "/proxy"
+process.env.PATH_PREFIX = ""
 process.env.PROXY_TEST_VALID = "http://localhost"
 process.env.PROXY_TEST_INVALID = "http://localhost:4000"
 process.env.PROXY_TEST_HTTPS = "https://api.users.maximemoreillon.com/"
+
+import request from "supertest"
+import { expect } from "chai"
+import { app } from ".."
 
 describe("/proxy", () => {
   before(async () => {
@@ -15,38 +15,34 @@ describe("/proxy", () => {
   describe("GET /proxy", () => {
     it("Should return app info", async () => {
       const { status } = await request(app).get("/proxy")
-
       expect(status).to.equal(200)
     })
   })
 
   describe("GET /", () => {
-    it("Should return GUI", async () => {
+    it("Should return the GUI", async () => {
       const { status } = await request(app).get("/")
-
       expect(status).to.equal(200)
     })
   })
 
-  describe("GET /proxy/test", () => {
+  describe("GET /test-valid", () => {
     it("Should proxy the test service", async () => {
-      const { status } = await request(app).get("/proxy/test-valid")
-
+      const { status } = await request(app).get("/test-valid")
       expect(status).to.equal(200)
     })
   })
 
-  describe("GET /proxy/unregistered", () => {
+  describe("GET /unregistered", () => {
     it("Should not allow proxying an unregistered service", async () => {
-      const { status } = await request(app).get("/proxy/unregistered")
-
+      const { status } = await request(app).get("/unregistered")
       expect(status).to.equal(404)
     })
   })
 
-  describe("GET /proxy/test", () => {
+  describe("GET /test", () => {
     it("Should not proxy a service which does not work", async () => {
-      const { status } = await request(app).get("/proxy/test-invalid")
+      const { status } = await request(app).get("/test-invalid")
 
       expect(status).to.equal(500)
     })
