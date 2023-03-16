6e327ea4efb8f7ea033159f98a409c4c38cc88b0
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-17 06:59:07 +0900

    more minor

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import apiMetrics from "prometheus-api-metrics"
-import { Request, Response, NextFunction, ErrorRequestHandler } from "express"
+import { ErrorRequestHandler } from "express"
 import { router } from "./routing"
 
 dotenv.config()
@@ -9,7 +9,6 @@ dotenv.config()
 const { PORT = 80 } = process.env
 
 // Express error handling
-// TODO: find type of error
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.error(err)
   const { statusCode = 500, message } = err
